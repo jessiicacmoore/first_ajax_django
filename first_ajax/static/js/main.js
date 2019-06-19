b4db08a55a066f983c1df7be577437915b1e755f
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-19 16:00:07 -0400

    Request to count

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,4 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const req = axios.get('http://intro-ajax-api.herokuapp.com/')
+  const btnRootReq = document.querySelector('.btn-root-req');
+  const btnPingReq = document.querySelector('.btn-ping-req');
+  const btnCountReq = document.querySelector('.btn-count-req');
+
+  btnRootReq.addEventListener('click', () => {
+    axios.get('http://intro-ajax-api.herokuapp.com/')
+    .then(resp => {
+      console.log(resp)
+    })
+  });
+
+  btnPingReq.addEventListener('click', () => {
+    axios.get('http://intro-ajax-api.herokuapp.com/ping')
+    .then(resp => {
+      console.log(resp.data);
+      dataP = document.createElement('p');
+      dataP.innerText = resp.data;
+      btnPingReq.parentElement.appendChild(dataP);
+    })
+    .catch(error => {
+      console.log(error.data)
+      dataP = document.createElement('p');
+      dataP.innerText = `Uh, oh. We messed up`;
+      btnPingReq.parentElement.appendChild(dataP);
+    })
+    .then(resp => {
+      console.log('Heyooo, the request is over!');
+    })
+  });
+
+  btnCountReq.addEventListener('click', () => {
+    axios.get('http://intro-ajax-api.herokuapp.com/count')
+    .then(resp => {
+      console.log(resp.data);
+      dataP = document.createElement('p');
+      dataP.innerText = resp.data;
+      btnCountReq.parentElement.appendChild(dataP);
+    })
+  })
   
 });
